1b26ab173a5c3c24b977a356d26425db9131deec
icer2312@yahoo.com.au <jarrad.obrien@cubesys.com.au>
2018-10-19 18:49:05 +1100

    Calculates the highest prime up to the specified number

--- ThesisSmartphoneApp/ThesisSmartphoneApp/ViewModels/CalculatingPrimes.cs
+++ ThesisSmartphoneApp/ThesisSmartphoneApp/ViewModels/CalculatingPrimes.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using Xamarin.Forms;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ThesisSmartphoneApp.ViewModels
 {
@@ -12,7 +12,8 @@ namespace ThesisSmartphoneApp.ViewModels
     {
 
         public int _largestPrime = 0;
-        public ICommand CalculatePrimesCommand { get; private set; }
+
+        public int _number;
 
         public int LargestPrime
         {
@@ -35,14 +36,61 @@ namespace ThesisSmartphoneApp.ViewModels
             }
         }
 
+        public int Number
+        {
+            set
+            {
+                if (_number != value)
+                {
+                    _number = value;
 
-        public Command CalculatePrimes
+                    if (PropertyChanged != null)
+                    {
+                        OnPropertyChanged();
+                    }
+                }
+            }
+
+            get
+            {
+                return _number;
+            }
+        }
+
+
+        public Command CalculateLargestPrimeCommand
         {
             get
             {
-                return new Command(() => { LargestPrime++; });
+                return new Command(() => { LargestPrime = CalculateLargestPrime(Number); });
+            }
+        }
+
+        public int CalculateLargestPrime(int calculateTo)
+        {
+            int largestPrime = 1;
+            bool isPrime;
+
+            for(int i = 1; i <= calculateTo; i++)
+            {
+                isPrime = true;
+
+                for(int j = 2; j <= i/2; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if(isPrime)
+                {
+                    largestPrime = i;
+                }
             }
 
+            return largestPrime;
         }
 
 
