69e13c01a5016c7649e3a42e45488f476ec56e9e
icer2312@yahoo.com.au <jarrad.obrien@cubesys.com.au>
2018-10-19 17:16:18 +1100

    Creates a button that increments a number on the home page

---
+++ ThesisSmartphoneApp/ThesisSmartphoneApp/ViewModels/CalculatingPrimes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace ThesisSmartphoneApp.ViewModels
+{
+    class CalculatingPrimes : INotifyPropertyChanged
+    {
+
+        public int _largestPrime = 0;
+        public ICommand CalculatePrimesCommand { get; private set; }
+
+        public int LargestPrime
+        {
+            set
+            {
+                if(_largestPrime != value)
+                {
+                    _largestPrime = value;
+
+                    if(PropertyChanged != null)
+                    {
+                        OnPropertyChanged();
+                    }
+                }
+            }
+
+            get
+            {
+                return _largestPrime;
+            }
+        }
+
+
+        public Command CalculatePrimes
+        {
+            get
+            {
+                return new Command(() => { LargestPrime++; });
+            }
+
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
