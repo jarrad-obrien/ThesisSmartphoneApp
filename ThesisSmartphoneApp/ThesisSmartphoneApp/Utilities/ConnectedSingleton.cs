dcc5f8fc0b3e1759448922379492692dce1b5349
Jarrad O'Brien <icer2312@yahoo.com.au>
2018-11-03 02:10:13 +1100

    Adds ConnectedSingleton

---
+++ ThesisSmartphoneApp/ThesisSmartphoneApp/Utilities/ConnectedSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThesisSmartphoneApp.Utilities
+{
+	class ConnectedSingleton
+	{
+		private static ConnectedSingleton instance;
+
+		private bool _connected = false;
+
+		private ConnectedSingleton() { }
+
+		public static ConnectedSingleton Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new ConnectedSingleton();
+
+				return instance;
+			}
+		}
+
+		public bool Connected
+		{
+			get
+			{
+				return _connected;
+			}
+		}
+	}
+}
